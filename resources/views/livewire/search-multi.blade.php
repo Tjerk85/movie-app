84526832b95904db968c408da10dd2cd2c2af3e4
Tjerk de Baat <tjerk.de.baat@gmail.com>
2025-01-22 20:27:34 +0100

    Added livewire component for searching movies,tv shows and actors

--- resources/views/livewire/search-multi.blade.php
+++ resources/views/livewire/search-multi.blade.php
@@ -24,10 +24,10 @@
            dark:focus:ring-blue-500
            dark:focus:border-blue-500"
            placeholder="E.g: Matrix reloaded"/>
-    <div>
-        @if(strlen($search) >= 2 && !is_null($results))
-            <ul x-show="! expanded"
-                class="absolute bg-gray-900 pl-4 pr-5 pb-2 rounded border-2 border-gray-500 mr-10 w-[450px] h-[600px] overflow-auto">
+
+    @if(strlen($search) >= 2 && !is_null($results))
+        <div x-show="! expanded" class="absolute bg-gray-900 pl-4 pr-5 pb-2 rounded border-2 border-gray-500 mr-10 w-[450px] h-[600px] overflow-auto">
+            <ul>
                 @foreach ($results as $result)
                     <li wire:key="{{ $result->id }}" class="p-2 border-gray-500 border-b-2">
                         <a href="{{ $result->medium }}" class="flex flex-row items-center">
@@ -41,11 +41,11 @@
                     </li>
                 @endforeach
             </ul>
-        @elseif(strlen($search) > 0)
-            <ul class="absolute bg-gray-900 pl-4 pr-5 pb-2 pt-3 rounded mr-10 overflow-auto">
-                <li>No results for:</li>
-                <li>{{ $search }}</li>
-            </ul>
-        @endif
-    </div>
+            @elseif(strlen($search) > 0)
+                <ul class="absolute bg-gray-900 pl-4 pr-5 pb-2 pt-3 rounded mr-10 overflow-auto">
+                    <li>No results for:</li>
+                    <li>{{ $search }}</li>
+                </ul>
+        </div>
+    @endif
 </div>
