049e380c68adab2475055788132d0322c580c98b
Tjerk de Baat <tjerk.de.baat@gmail.com>
2025-02-05 12:37:29 +0100

    Styling the genre menu

--- resources/views/components/dropdown-menu.blade.php
+++ resources/views/components/dropdown-menu.blade.php
@@ -1,4 +1,4 @@
-<div class="group-hover:block dropdown-menu absolute hidden h-auto border-2 border-gray-800">
+<div class="group-hover:block dropdown-menu absolute hidden h-auto border-2 border-gray-800 {{$class}}">
     <ul class="top-0 w-48 border-gray-500 bg-gray-900 shadow px-6 py-8 rounded">
         <li><livewire:genres :typeOfMedia="$typeOfMedia" /></li>
     </ul>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,11 +2,11 @@
     <ul class="max-w-full md:ml-20 md:mr-20 flex flex-wrap items-center justify-between mx-auto p-4">
         <li class="group relative dropdown px-4 cursor-pointer font-bold text-base uppercase tracking-wide">
             <a href="{{ route('home') }}">Movies</a>
-            <x-dropdown-menu :typeOfMedia="'movie'" />
+            <x-dropdown-menu :class="'left-6'" :typeOfMedia="'movie'" />
         </li>
         <li class="group relative dropdown px-4 cursor-pointer font-bold text-base uppercase tracking-wide">
             <a href="{{ route('tv') }}">Tv</a>
-            <x-dropdown-menu :typeOfMedia="'tv'" />
+            <x-dropdown-menu :class="'right-0'" :typeOfMedia="'tv'" />
         </li>
         <li class="font-bold text-base uppercase tracking-wide">
             <a href="{{ route('actors') }}">Actors</a>
